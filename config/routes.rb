5f2e184393f047474c95aac37f2069dce53de4c4
thecookieorg <marko@thecookie.org>
2016-06-14 18:18:33 -0700

    handling errors implemented for unknown cart names

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :products
   devise_for :users
 
-  root 'store#index'
+  root 'store#index', as: 'store'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
